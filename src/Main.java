5d8b3625b91aea505e58eb045a59b46d87dc2cf6
Marina Robionek <marinarobionek@gmail.com>
2023-11-29 11:31:29 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 import java.io.File;
 import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "/Users/marinarobionek/Desktop/Обучение";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
-    }
 
-    public static long getFolderSize(File folder) {
-        if(folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        return sum = Arrays.stream(files)
-                .mapToLong(f -> getFolderSize(f))
-                .sum();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
     }
 }
